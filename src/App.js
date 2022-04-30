5819c0f3921a99f4f0b2275c98e0b7e09f4eceb8
Vitor <vitoraugusto002@hotmail.com>
2022-04-29 23:34:33 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>text</Text>
+    </View>
+  );
+};
+
+export default App;
