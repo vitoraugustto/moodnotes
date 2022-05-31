4379141f0726c54e424ebb7ad90bbf55ebc2bc58
Vitor <vitoraugusto002@hotmail.com>
2022-05-31 17:50:38 -0300

    create ads with admob

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,25 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { store, persistor } from './store';
 
+import {
+  AppOpenAd,
+  InterstitialAd,
+  RewardedAd,
+  BannerAd,
+  TestIds,
+  BannerAdSize,
+} from 'react-native-google-mobile-ads';
+
 import Routes from './routes';
 
 const App = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+        <BannerAd
+          sizes={[BannerAdSize.FULL_BANNER]}
+          unitId="ca-app-pub-2882287196167600/7666393913"
+        />
         <Routes />
       </PersistGate>
     </Provider>
